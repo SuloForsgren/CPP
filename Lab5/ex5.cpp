506572faf5fa7197ea0fafe15b933f9bd85d316d
SuloForsgren <sulo.forsgren@gmail.com>
2024-09-03 13:09:18 +0300

    Workadoodles

--- Lab5/ex5.cpp
+++ Lab5/ex5.cpp
@@ -1,11 +1,72 @@
-#include <iostream>
+#include <iostream>  
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
 
 using namespace std;
 
+// Ordinary function
+/*int randGen_1() {
+    return rand() % 10 + 1;
+}*/
+
+// Function object
+class RandGen_2 {
+public:
+    RandGen_2(const int min, const int max) : min(min), max(max), numbers() {}
+    int operator()();
+private:
+    vector<int> numbers;
+    const int min, max;
+};
+
+int RandGen_2::operator()() {
+    int number;
+    do {
+        number = rand() % (max - min + 1) + min;
+    } 
+    while (find(numbers.begin(), numbers.end(), number) != numbers.end());
+    numbers.push_back(number);
+    return number;
+}
+
 int main() {
+    srand(time(NULL)); 
 
+    vector<int> lotto(10);
+    vector<int> vikingLotto(10);
+    vector <int> euroJackpot(10);
+    
+    RandGen_2 genLotto(1, 40);
+    RandGen_2 genVikingLotto(1, 48);
+    RandGen_2 genEurojackpot(1, 50);
 
+    generate(lotto.begin(), lotto.end(), genLotto);
+    generate(vikingLotto.begin(), vikingLotto.end(), genVikingLotto);
+    generate(euroJackpot.begin(), euroJackpot.end(), genEurojackpot);
 
+    cout << "Lotto: ";
+    copy(lotto.begin(), lotto.end(), ostream_iterator<int>(cout, " "));
+    cout << std::endl;
 
+    cout << "Viking lotto: ";
+    copy(vikingLotto.begin(), vikingLotto.end(), ostream_iterator<int>(cout, " "));
+    cout << std::endl;
+
+    cout << "Eurojackpot: ";
+    copy(euroJackpot.begin(), euroJackpot.end(), ostream_iterator<int>(cout, " "));
+    cout << std::endl;
+
+    vector<int> intersection(min(lotto.size(), vikingLotto.size()));
+    auto it = set_intersection(lotto.begin(), lotto.end(), vikingLotto.begin(), vikingLotto.end(), intersection.begin());
+    intersection.resize(it - intersection.begin());
+
+    for (auto number : intersection) {
+        cout << number << " ";
+    }
+    cout << endl;
+    
     return 0;
 }

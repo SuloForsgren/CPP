a430b65ebbec74514172f1b481b53a1f248a8269
SuloForsgren <sulo.forsgren@gmail.com>
2024-09-08 13:24:40 +0300

    Perkele

--- Lab5/ex5.cpp
+++ Lab5/ex5.cpp
@@ -37,38 +37,55 @@ void printMatching(int element) {
 }
 
 int main() {
-    srand(time(NULL)); 
+    bool runAgain = true;
+    while (runAgain) {
+        srand(time(NULL)); 
 
-    vector<int> lotto(10);
-    vector<int> vikingLotto(10);
-    vector <int> euroJackpot(10);
-    
-    RandGen_2 genLotto(1, 10);
-    RandGen_2 genVikingLotto(1, 10);
-    RandGen_2 genEurojackpot(1, 50);
+        vector<int> lotto(10);
+        vector<int> vikingLotto(10);
+        vector <int> euroJackpot(10);
+        
+        RandGen_2 genLotto(1, 10);
+        RandGen_2 genVikingLotto(1, 10);
+        RandGen_2 genEurojackpot(1, 50);
 
-    generate(lotto.begin(), lotto.end(), genLotto);
-    generate(vikingLotto.begin(), vikingLotto.end(), genVikingLotto);
-    generate(euroJackpot.begin(), euroJackpot.end(), genEurojackpot);
+        generate(lotto.begin(), lotto.end(), genLotto);
+        generate(vikingLotto.begin(), vikingLotto.end(), genVikingLotto);
+        generate(euroJackpot.begin(), euroJackpot.end(), genEurojackpot);
 
-    cout << "Lotto: ";
-    copy(lotto.begin(), lotto.end(), ostream_iterator<int>(cout, " "));
-    cout << std::endl;
+        cout << "Lotto: ";
+        copy(lotto.begin(), lotto.end(), ostream_iterator<int>(cout, " "));
+        cout << std::endl;
 
-    cout << "Viking lotto: ";
-    copy(vikingLotto.begin(), vikingLotto.end(), ostream_iterator<int>(cout, " "));
-    cout << std::endl;
+        cout << "Viking lotto: ";
+        copy(vikingLotto.begin(), vikingLotto.end(), ostream_iterator<int>(cout, " "));
+        cout << std::endl;
 
-    cout << "Eurojackpot: ";
-    copy(euroJackpot.begin(), euroJackpot.end(), ostream_iterator<int>(cout, " "));
-    cout << std::endl;
+        cout << "Eurojackpot: ";
+        copy(euroJackpot.begin(), euroJackpot.end(), ostream_iterator<int>(cout, " "));
+        cout << std::endl;
 
-    vector<int> intersection(min(lotto.size(), vikingLotto.size()));
-    auto it = set_intersection(lotto.begin(), lotto.end(), vikingLotto.begin(), vikingLotto.end(), intersection.begin());
-    intersection.resize(it - intersection.begin());
+        vector<int> intersection(min(lotto.size(), vikingLotto.size()));
+        auto it = set_intersection(lotto.begin(), lotto.end(), vikingLotto.begin(), vikingLotto.end(), intersection.begin());
+        intersection.resize(it - intersection.begin());
 
-    for_each(intersection.begin(), intersection.end(), printMatching);
-    cout << endl;
-    
+        for_each(intersection.begin(), intersection.end(), printMatching);
+        cout << endl;
+        
+        string again;
+        cout << "Do you want to play again? (yes/no): ";
+        cin >> again;
+
+        if (again == "yes") {
+            runAgain = true;
+        }
+        else if (again == "no") {
+            runAgain = false;
+        }
+        else {
+            runAgain = false;
+        }
+    }
+
     return 0;
 }
